4d0f3ea06e770ee1136be1c3b2e12e76b2b764b1
ZeNeto10 <jrsneto.2005@gmail.com>
2024-11-15 12:53:22 -0300

    Atualizando as Funções

--- CALCULO/script_calculo.sql
+++ CALCULO/script_calculo.sql
@@ -1,13 +1,41 @@
-CREATE OR REPLACE FUNCTION FN_VALIDAR_CEP(
-    p_cep VARCHAR2
-) RETURN VARCHAR2 IS
+CREATE OR REPLACE FUNCTION validar_email (p_email IN VARCHAR2) 
+RETURN VARCHAR2 IS
+    v_pattern VARCHAR2(100) := '^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,}$'; -- Expressão regular para validar o formato do e-mail
 BEGIN
-    IF REGEXP_LIKE(p_cep, '^\d{5}-\d{3}$') THEN
-        RETURN 'Válido';
-    ELSE
-        RETURN 'Inválido';
+    -- Verifica se o e-mail corresponde ao padrão usando REGEXP_LIKE
+    IF NOT REGEXP_LIKE(p_email, v_pattern) THEN
+        -- Se o formato for inválido, gera uma exceção
+        RAISE_APPLICATION_ERROR(-20001, 'Formato de e-mail inválido');
     END IF;
+    
+    -- Se o formato for válido, retorna uma mensagem de sucesso
+    RETURN 'E-mail válido';
+EXCEPTION
+    WHEN OTHERS THEN
+        -- Tratamento de exceção
+        RETURN 'Erro: ' || SQLERRM;
 END;
+
+
+CREATE OR REPLACE FUNCTION validar_cep (p_cep IN VARCHAR2) 
+RETURN VARCHAR2 IS
+    v_pattern VARCHAR2(100) := '^\d{5}-\d{3}$'; -- Expressão regular para validar o formato do CEP (ex: 12345-678)
+BEGIN
+    -- Verifica se o CEP corresponde ao padrão usando REGEXP_LIKE
+    IF NOT REGEXP_LIKE(p_cep, v_pattern) THEN
+        -- Se o formato for inválido, gera uma exceção
+        RAISE_APPLICATION_ERROR(-20003, 'Formato de CEP inválido');
+    END IF;
+    
+    -- Se o formato for válido, retorna uma mensagem de sucesso
+    RETURN 'CEP válido';
+EXCEPTION
+    WHEN OTHERS THEN
+        -- Tratamento de exceção
+        RETURN 'Erro: ' || SQLERRM;
+END;
+
+
 
 
 CREATE OR REPLACE FUNCTION FN_CALCULAR_CONSUMO_DIARIO(
