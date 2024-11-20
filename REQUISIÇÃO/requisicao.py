957671c049cf8ff1c142ef4765977a4ba5a8183b
ZeNeto10 <jrsneto.2005@gmail.com>
2024-11-20 18:28:16 -0300

    Alterando nomeclaturas

--- REQUISIÇÃO/requisicao.py
+++ REQUISIÇÃO/requisicao.py
@@ -3,13 +3,13 @@ import json
 import pandas as pd
 from datetime import datetime
 
-# Função para criar conexão
+
 def criar_conexao():
     try:
-        # Defina a string de conexão (DSN)
+        
         dsnStr = oracledb.makedsn("oracle.fiap.com.br", 1521, "ORCL")
         
-        # Estabelece a conexão
+        
         conn = oracledb.connect(
             user="rm553621",
             password="051102",
@@ -23,23 +23,23 @@ def criar_conexao():
         print("Erro ao conectar ao banco:", e)
         return None
 
-# Função para tratar datetime e convertê-los para string
+
 def converter_datetime(dados):
     for tabela in dados:
         for i, registro in enumerate(dados[tabela]):
             for chave, valor in registro.items():
-                if isinstance(valor, datetime):  # Verifica se o valor é do tipo datetime
-                    registro[chave] = valor.strftime('%Y-%m-%d %H:%M:%S')  # Converte para string
+                if isinstance(valor, datetime):  
+                    registro[chave] = valor.strftime('%Y-%m-%d %H:%M:%S')  
     return dados
 
-# Função para exportar dados de todas as tabelas para JSON
+
 def exportar_para_json():
     conn = criar_conexao()
     
     if conn is None:
         return
 
-    # Consultas SQL para extrair dados de todas as tabelas
+   
     tabelas = [
         "T_PE_USUARIOS",
         "T_PE_ENDERECOS",
@@ -55,20 +55,20 @@ def exportar_para_json():
     try:
         cursor = conn.cursor()
         
-        # Executa a consulta para cada tabela e armazena o resultado
+        
         for tabela in tabelas:
             query = f"SELECT * FROM {tabela}"
             cursor.execute(query)
-            colunas = [col[0] for col in cursor.description]  # Obtém os nomes das colunas
-            registros = cursor.fetchall()  # Obtém todos os registros
+            colunas = [col[0] for col in cursor.description]  
+            registros = cursor.fetchall()  
 
             # Converte os dados em um dicionário
             dados[tabela] = [dict(zip(colunas, registro)) for registro in registros]
         
-        # Converte qualquer campo datetime para string
+        
         dados = converter_datetime(dados)
         
-        # Salva os dados em um arquivo JSON
+
         with open('dados_exportados.json', 'w', encoding='utf-8') as f:
             json.dump(dados, f, ensure_ascii=False, indent=4)
         
@@ -81,5 +81,4 @@ def exportar_para_json():
         cursor.close()
         conn.close()
 
-# Chama a função para exportar os dados
 exportar_para_json()
